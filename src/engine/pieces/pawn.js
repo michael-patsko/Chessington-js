852c6c752c81a6ad10a08e6a1b7d6ebe1b37dca1
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-04 14:01:08 +0100

    add handling for pawn piece moves

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,10 +9,25 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
+
+        // Handles first move
+        if (this.player === Player.WHITE && location.row === 1) {
+            return [
+                Square.at(location.row + 1, location.col),
+                Square.at(location.row + 2, location.col),
+            ];
+        } else if (this.player === Player.BLACK && location.row === 6) {
+            return [
+                Square.at(location.row - 1, location.col),
+                Square.at(location.row - 2, location.col),
+            ]
+        }
+
+        // Handles all other moves when not capturing
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
+            return [Square.at(location.row + 1, location.col)];
         } else {
-            return Square.at(location.row - 1, location.col)
+            return [Square.at(location.row - 1, location.col)];
         }
     }
 }
