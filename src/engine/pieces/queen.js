3da4aa23ef2c92d848dd49212b268e1ec1e4ce21
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-04 15:39:36 +0100

    added movement functionality for the queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../Square";
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,28 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let newMoves = [];
+        
+        // For diagonal movement
+        for (let a = 1; a < 8; a++) {
+            const positiveRow = location.row + a;
+            const negativeRow = location.row - a;
+            const positiveCol = location.col + a;
+            const negativeCol = location.col - a;
+            if (positiveRow >= 0 && positiveRow < 8 && positiveCol >=0 && positiveCol < 8) newMoves.push(Square.at(positiveRow, positiveCol));
+            if (positiveRow >= 0 && positiveRow < 8 && negativeCol >=0 && negativeCol < 8) newMoves.push(Square.at(positiveRow, negativeCol));
+            if (negativeRow >= 0 && negativeRow < 8 && positiveCol >=0 && positiveCol < 8) newMoves.push(Square.at(negativeRow, positiveCol));
+            if (negativeRow >= 0 && negativeRow < 8 && negativeCol >=0 && negativeCol < 8) newMoves.push(Square.at(negativeRow, negativeCol));
+        }
+        
+        // For lateral movement
+        for (let i = 0; i < 8; i++) {
+            if (i != location.row) newMoves.push(Square.at(i, location.col));
+        }
+        for (let j = 0; j < 8; j++) {
+            if (j != location.col) newMoves.push(Square.at(location.row, j));
+        }
+        return newMoves;
     }
 }
