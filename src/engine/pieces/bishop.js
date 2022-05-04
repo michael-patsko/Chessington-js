6d9cb06596e4371caf2d70405347f884903d732a
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-04 15:36:03 +0100

    added functionality for bishop moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../Square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,18 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let newMoves = [];
+        for (let a = 1; a < 8; a++) {
+            const positiveRow = location.row + a;
+            const negativeRow = location.row - a;
+            const positiveCol = location.col + a;
+            const negativeCol = location.col - a;
+            if (positiveRow >= 0 && positiveRow < 8 && positiveCol >=0 && positiveCol < 8) newMoves.push(Square.at(positiveRow, positiveCol));
+            if (positiveRow >= 0 && positiveRow < 8 && negativeCol >=0 && negativeCol < 8) newMoves.push(Square.at(positiveRow, negativeCol));
+            if (negativeRow >= 0 && negativeRow < 8 && positiveCol >=0 && positiveCol < 8) newMoves.push(Square.at(negativeRow, positiveCol));
+            if (negativeRow >= 0 && negativeRow < 8 && negativeCol >=0 && negativeCol < 8) newMoves.push(Square.at(negativeRow, negativeCol));
+        }
+        return newMoves;
     }
 }
